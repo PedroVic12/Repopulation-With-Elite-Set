fb1f733ae9b45fc3021c34fd777212fefecd7d49
Pedro Victor Veras <pedrovictorveras@id.uff.br>
2025-04-08 14:02:11 -0300

    up

--- app/modules/streamlit_app_MVVM/app.py
+++ app/modules/streamlit_app_MVVM/app.py
@@ -13,7 +13,7 @@ def load_css(file_name: str):
 class App:
     def __init__(self):
         st.set_page_config(
-            page_title="Dashboard Interativo 2025 - RCE",
+            page_title="Dashboard Interativo 2025 - RCE V5",
             page_icon="📊",
             layout="wide", 
             initial_sidebar_state="expanded"
