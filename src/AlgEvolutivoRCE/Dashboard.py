e626e02df4422b6c73c8cc6fbf77e406071f2e15
Pedro Victor Veras <pedrovictorveras@id.uff.br>
2025-05-07 13:37:32 -0300

    Update Dashboard.py

--- src/AlgEvolutivoRCE/Dashboard.py
+++ src/AlgEvolutivoRCE/Dashboard.py
@@ -1,9 +1,12 @@
 
 # gera o arquivo para rodar dentro do colab
 
+#%%writefile app.py
+
 import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
+
 import pickle
 import plotly.io as pio
 import os
@@ -114,54 +117,65 @@ class DashboardApp:
 
         self.plot_Rastrigin_2D(X, Y, Z_3D_rastrigin, logbook, best)
 
-    def graficoRCE(self, gen, lista, fig= None, repopulation=False):
+    def graficoRCE(self, gen, lista,  repopulation=False):
         title = f"Estrategia RCE - Crossover: {params['CROSSOVER']*100}% e Mutação: {params['MUTACAO']*100}% " if repopulation else "Sem Repopulação RCE"
 
-        fig = go.Figure() if fig is None else fig  # Create new figure if None
+        # Create a new figure before plotting
+        figure = go.Figure()   # Create new figure if None
 
-
-        fig.add_trace(
-            go.Scatter(
+        figure.add_trace(
+            go.Line(
                 x=gen,
-                y=lista["min_fitness"],
+                #y=lista["min_fitness"],
+                y = lista[0],
                 mode="lines+markers",
                 name="Valor Min Fitness",
                 marker=dict(symbol="star", color="blue"),
                 line=dict(color="blue"),
             )
         )
+        
 
-        fig.add_trace(
-            go.Scatter(
+        figure.add_trace(
+            go.Line(
                 x=gen,
-                y=lista["avg_fitness"],
+                #y=lista["avg_fitness"],
+                y = lista[1],
+
                 mode="lines+markers",
                 name="Média Fitness",
                 marker=dict(symbol="cross", color="red"),
                 line=dict(color="red"),
             )
         )
-
-        fig.add_trace(
-            go.Scatter(
+        
+        
+        figure.add_trace(
+            go.Line(
                 x=gen,
-                y=lista["max_fitness"],
+                #y=lista["max_fitness"],
+                y = lista[2],
+
                 mode="lines+markers",
                 name="Valor Max Fitness",
                 marker=dict(symbol="circle", color="green"),
                 line=dict(color="green"),
             )
         )
+       
 
-        fig.update_layout(
+        figure.update_traces(overwrite= True)
+
+        figure.update_layout(
             title=title,
             xaxis_title="Generation",
             yaxis_title="Fitness",
-            legend_title="Legend",
-            template="plotly_white",
-        )
+            legend_title="Algoritimo Evolutivo",
+            template="plotly", #      ['ggplot2', 'seaborn', 'simple_white', 'plotly', 'plotly_white', ...]
+            overwrite= True
+            )
 
-        return fig
+        return figure
 
     # --- visualize MODIFICADO (salva dados e chama Streamlit) ---
     def visualize(self, logbook, pop, repopulation=True, DEBUG=False, current_params=None, execution_num=1):
@@ -171,34 +185,11 @@ class DashboardApp:
         """
         generation = []
         statics = {}
+        array_values = []
+
         best_solution_index = -1
         best_solution_variables = []
         best_solution_fitness = float('inf')
-
-        # --- MODIFIED: Define filenames based on execution_num ---
-        if execution_num is None:
-            # Decide fallback behavior or raise error if number is always required
-            # Option 1: Raise error (safer if logic depends on it)
-            print("WARN: execution_num not provided. Using default filenames.")
-
-            raise ValueError("Execution number (execution_num) must be provided to visualize for saving files.")
-
-        else:
-
-            # check se o diretorio output exists
-            if not os.path.exists("./output"):
-                os.makedirs("./output")
-                print("Diretório 'output' criado com sucesso.")
-            else:
-                print("Diretório 'output' já existe.")
-
-            data_file = f"./output/dashboard_data_{execution_num}.pkl"
-            fig_file = f"./output/dashboard_fig_{execution_num}.json"
-            print(f"INFO: Arquivos de saída para execução {execution_num}: {data_file}, {fig_file}")
-
-
-
-        # --- END MODIFICATION ---
 
         try:
             generation = logbook.select("gen")
@@ -206,12 +197,26 @@ class DashboardApp:
 
             if DEBUG:
                 print("\n\nDEBUG: Dados para gráfico (generation x statics)")
-                print(statics)
+                print(len(statics["min_fitness"])) # DEBGU = 90
 
             min_fitness_values = statics.get("min_fitness", [])
+            #print(len(min_fitness_values))
             if not min_fitness_values: raise ValueError("Min fitness list is empty")
             best_solution_fitness = min(min_fitness_values)
             best_solution_index = min_fitness_values.index(best_solution_fitness)
+
+            
+            # Foi feito os SLICING  porque o deap acumula os resultados  de fitness na varaiveal statics
+            array_values.append(
+                statics.get("min_fitness", [])[(execution_num -1) * int(len(generation)/execution_num):(execution_num * int(len(generation)/execution_num)) -1] 
+            )
+            array_values.append(
+                statics.get("avg_fitness", [])[(execution_num -1) * int(len(generation)/execution_num):(execution_num * int(len(generation)/execution_num)) -1] 
+            )
+            array_values.append(
+                statics.get("max_fitness", [])[(execution_num -1) * int(len(generation)/execution_num):(execution_num * int(len(generation)/execution_num)) -1] 
+            )
+
 
             if repopulation:
                 best_solution_variables = pop[0] if pop else []
@@ -219,45 +224,70 @@ class DashboardApp:
                 print("WARN: Visualize - Lógica para 'best_solution_variables' sem repopulação usa fallback.")
                 best_solution_variables = pop[0] if pop else []
 
+            print("\n\n")
             print("="*90)
-            print(f"  >>> Soluções do problema (Execução {execution_num} - Console Output) <<<") # Added execution num here
+            print(f"  >>> Soluções do problema (Execução {execution_num} - Console Output) <<<")
             print("="*90)
             print(f"Best Generation: {best_solution_index}")
             print(f"Best Variables: {best_solution_variables}")
             print(f"Best Fitness: {best_solution_fitness}")
             print("="*90)
 
-            # Create a new figure before plotting
-            fig = self.graficoRCE(generation, statics, repopulation=repopulation) 
+            
+
+            grafico_RCE = self.graficoRCE(generation, array_values, repopulation=repopulation)
 
             # Exibir a figura no notebook
-            fig.show()
+            #fig.show()
+
+             # --- MODIFIED: Define filenames based on execution_num ---
+            if execution_num is None:
+                # Decide fallback behavior or raise error if number is always required
+                # Option 1: Raise error (safer if logic depends on it)
+                print("WARN: execution_num not provided. Using default filenames.")
+
+                raise ValueError("Execution number (execution_num) must be provided to visualize for saving files.")
+
+            else:
+
+                # check se o diretorio output exists
+                if not os.path.exists("./output"):
+                    os.makedirs("./output")
+                    print("Diretório 'output' criado com sucesso.")
+                else:
+                    print("Diretório 'output' já existe.")
+
+                data_file = f"./output/dashboard_data_{execution_num}.pkl"
+                fig_file = f"./output/dashboard_fig_{execution_num}.json"
+                print(f"INFO: Arquivos de saída para execução {execution_num}: {data_file}, {fig_file}")
+
+                # --- Salvar dados e figura para o script Streamlit ---
+                print(f"INFO: Salvando dados para visualizador Streamlit (Execução {execution_num})...\n") # Added execution num here
+                data_to_save = {
+                    'execution_num': execution_num, # Store execution number in data
+                    'best_gen_idx': best_solution_index,
+                    'best_vars': best_solution_variables,
+                    'best_fitness': best_solution_fitness,
+                    'params': current_params if current_params else params,
+                    'logbook_data': {
+                        'generation': generation,
+                        'statics': statics
+                    }
+                }
 
             # Salvar a figura em formato HTML
             fig_filename = f"output/grafico_execucao_{execution_num}.html"
-            fig.write_html(fig_filename)
-
+            grafico_RCE.write_html(fig_filename)
             print(f"INFO: Figura salva em {fig_filename}")
 
-            # --- Salvar dados e figura para o script Streamlit ---
-            print(f"INFO: Salvando dados para visualizador Streamlit (Execução {execution_num})...\n") # Added execution num here
-            data_to_save = {
-                'execution_num': execution_num, # Store execution number in data
-                'best_gen_idx': best_solution_index,
-                'best_vars': best_solution_variables,
-                'best_fitness': best_solution_fitness,
-                'params': current_params if current_params else params,
-                'logbook_data': {
-                    'generation': generation,
-                    'statics': statics
-                }
-            }
 
             # Salva os dados no arquivo .pkl numerado
             with open(data_file, 'wb') as f:
                 pickle.dump(data_to_save, f)
+
+
             # Salva a figura no arquivo .json numerado
-            fig_json = pio.to_json(fig)
+            fig_json = pio.to_json(grafico_RCE)
             with open(fig_file, 'w') as f:
                 f.write(fig_json)
             print(f"INFO: Dados e figura para execução {execution_num} salvos com sucesso.") # Added execution num here
@@ -266,7 +296,7 @@ class DashboardApp:
             print(f"ERRO em visualize (Execução {execution_num}): {e}") # Added execution num here
             return -1, [], float('inf'), None
 
-        return best_solution_index, best_solution_variables, best_solution_fitness, fig
+        return best_solution_index, best_solution_variables, best_solution_fitness, grafico_RCE
 
     def calculate_stats(self, logbook):
 
