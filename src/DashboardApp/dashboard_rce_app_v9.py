4935a632bb88ebf59dd9d2a3826e379aaa5e2867
Pedro Victor Veras <86419935+PedroVic12@users.noreply.github.com>
2025-05-28 00:17:42 -0300

    Update dashboard_rce_app_v9.py

--- src/DashboardApp/dashboard_rce_app_v9.py
+++ src/DashboardApp/dashboard_rce_app_v9.py
@@ -101,7 +101,7 @@ def DrawerSideBar():
         key="main_nav_radio"
     )
 
-    st.query_params(page=selected_page)
+    st.experimental_get_query_params(page=selected_page)
 
     st.sidebar.markdown("---")
     st.sidebar.info("Select a page above to view its content.")
@@ -140,7 +140,7 @@ if __name__ == "__main__":
     app = App()  # Initialize app config and styling
 
     # Obter parâmetros de URL
-    query_params = st.query_params()
+    query_params = st.experimental_get_query_params()
     selected_page = query_params.get("page", ["framework_rce"])[0]
 
     # Renderizar a página selecionada
