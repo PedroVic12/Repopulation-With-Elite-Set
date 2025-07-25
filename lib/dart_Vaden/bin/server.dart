e1a9313ca27abbdb64873734d767438c034094e1
Pedro Victor Veras <pedrovictor.rveras12@gmail.com>
2025-07-25 00:26:22 -0300

    Update server.dart

--- lib/dart_Vaden/bin/server.dart
+++ lib/dart_Vaden/bin/server.dart
@@ -1,9 +1,15 @@
 import 'package:kyogre_vaden_getx/vaden_application.dart';
 
 Future<void> main(List<String> args) async {
-  final vaden = VadenApp();
-  await vaden.setup();
-  final server = await vaden.run(args);
-  print('Server listening on port ${server.port}');
+  try {
+    print("Starting Vaden application...");
+    final vaden = VadenApp();
+    await vaden.setup();
+    final server = await vaden.run(args);
+    print('Server listening on port ${server.port}');
+  } on Exception catch (e) {
+    print('Error starting Vaden application: $e');
+  }
+  print("Vaden application started successfully.");
+  print("Press Ctrl+C to stop the server.");
 }
-
