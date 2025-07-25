ac7198c904327a485cdef1b05e853f239163e055
Pedro Victor Veras <pedrovictor.rveras12@gmail.com>
2025-07-25 19:17:33 -0300

    Update main.dart

--- lib/UI/dart_vaden_app/lib/main.dart
+++ lib/UI/dart_vaden_app/lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'My App',
       debugShowCheckedModeBanner: false,
       darkTheme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
