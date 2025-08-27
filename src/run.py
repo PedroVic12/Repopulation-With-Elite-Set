f783801fbb49f2bac406d65bb48386377f00048d
Pedro Victor Veras <pedrovictor.rveras12@gmail.com>
2025-08-27 13:35:55 -0300

    up

--- src/run.py
+++ src/run.py
@@ -64,6 +64,7 @@ if CLI:
     debug_mode = input("Deseja usar o modo debug? (S/N): default (N) ")
     DEBUG_MODE = True if debug_mode.lower() == "s" else False
 
+#! https://budavariam.github.io/asciiart-text/
 MSG_TERMINAL ="""
  __       _______ .___________. __      _______.   .______        ______     ______  __  ___  __  
 |  |     |   ____||           |(_ )    /       |   |   _  \      /  __  \   /      ||  |/  / |  | 
