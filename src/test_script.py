f08d95738d6445369bc01eb6f4ccd38e07682ff8
Pedro Victor Veras <pedrovictor.rveras12@gmail.com>
2025-12-11 10:37:44 -0300

    up

--- src/test_script.py
+++
@@ -1,7 +0,0 @@
-# src/test_script.py
-import time
-
-print("--- test_script.py: Este é um script de teste ---")
-print("--- test_script.py: Ele foi chamado via os.system() ---")
-time.sleep(1) # Simula algum trabalho
-print("--- test_script.py: Finalizando o script de teste ---")
